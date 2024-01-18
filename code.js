b4415327e849986ff7a7bfccb4d15931bbdf2f0a
Sandeep Singh <sandeep.socialpixe@gmail.com>
2024-01-18 13:00:45 +0530

    updated

--- code.js
+++ code.js
@@ -837,22 +837,22 @@ let nextObj_beData = [
   }
 ];
 
-function //bseDataPuller() {
-  var settings = {
-  "url": "https://api.bseindia.com/BseIndiaAPI/api/DerivOptionChain_IV/w?Expiry=19+Jan+2024&scrip_cd=1&strprice=0",
-  "method": "GET",
-  "timeout": 0,
-  "headers": {
-    "Origin": "https://www.bseindia.com",
-    "Referer": "https://www.bseindia.com/",
-    "Sec-Fetch-Dest": "empty",
-    "Sec-Fetch-Mode": "cors",
-    "Sec-Fetch-Site": "same-site",
-    "TE": "trailers"
-  },
-};
+// function //bseDataPuller() {
+//   var settings = {
+//   "url": "https://api.bseindia.com/BseIndiaAPI/api/DerivOptionChain_IV/w?Expiry=19+Jan+2024&scrip_cd=1&strprice=0",
+//   "method": "GET",
+//   "timeout": 0,
+//   "headers": {
+//     "Origin": "https://www.bseindia.com",
+//     "Referer": "https://www.bseindia.com/",
+//     "Sec-Fetch-Dest": "empty",
+//     "Sec-Fetch-Mode": "cors",
+//     "Sec-Fetch-Site": "same-site",
+//     "TE": "trailers"
+//   },
+// };
 
-$.ajax(settings).done(function (response) {
-  console.log(response);
-});
-}
+// $.ajax(settings).done(function (response) {
+//   console.log(response);
+// });
+// }
